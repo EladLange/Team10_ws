e8d5649dab5d7b7c82126110f6773c9c7d123fd1
Team10Ariel <team10.car@gmail.com>
2025-04-23 17:07:16 +0300

    replace Rode -> road

---
+++ src/vo/src/road.cpp
@@ -0,0 +1,24 @@
+#include "road.hpp"
+
+Road::Road(int num_lanes, double lane_width, double length)
+    : num_lanes_(num_lanes), lane_width_(lane_width), length_(length) {}
+
+std::vector<double> Road::getLaneCenterlines() const {
+    std::vector<double> lanes;
+    for (int i = 0; i < num_lanes_; ++i) {
+        lanes.push_back((i + 0.5) * lane_width_);
+    }
+    return lanes;
+}
+
+int Road::getNumLanes() const {
+    return num_lanes_;
+}
+
+double Road::getLaneWidth() const {
+    return lane_width_;
+}
+
+double Road::getLength() const {
+    return length_;
+}
