8fcfb918e0c10cda01157b9411377653c394482e
Team10Ariel <team10.car@gmail.com>
2025-04-23 11:46:11 +0300

    add velocity_obstacle & car src, include from old git

---
+++ src/vo/src/car.cpp
@@ -0,0 +1,52 @@
+#include "car.hpp"
+#include <cmath>
+#include <iostream>
+
+Car::Car(const std::string& id, bool is_controlled)
+    : id_(id), controlled_(is_controlled) {
+    pose_.position.x = 0.0;
+    pose_.position.y = 0.0;
+    pose_.position.z = 0.0;
+
+    pose_.orientation.w = 1.0;
+    pose_.orientation.x = 0.0;
+    pose_.orientation.y = 0.0;
+    pose_.orientation.z = 0.0;
+
+    velocity_.linear.x = 0.0;
+    velocity_.angular.z = 0.0;
+}
+
+void Car::setPose(const geometry_msgs::msg::Pose& pose) {
+    pose_ = pose;
+}
+
+void Car::setVelocity(const geometry_msgs::msg::Twist& vel) {
+    velocity_ = vel;
+}
+
+const geometry_msgs::msg::Pose& Car::getPose() const {
+    return pose_;
+}
+
+const geometry_msgs::msg::Twist& Car::getVelocity() const {
+    return velocity_;
+}
+
+std::string Car::getId() const {
+    return id_;
+}
+
+bool Car::isControlled() const {
+    return controlled_;
+}
+
+void Car::update(double dt) {
+    pose_.position.x += velocity_.linear.x * dt;
+    pose_.position.y += velocity_.linear.y * dt;
+    if (pose_.position.x>100.0){
+    std::cout<<id_<<" Is out of bounds"<<"\n";//debug
+    pose_.position.x=0;
+    }
+    pose_.orientation.z+=velocity_.angular.z * dt;
+}
