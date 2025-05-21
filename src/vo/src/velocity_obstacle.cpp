65c99ed6ba94a2a9143fe89dace5e7cb565d3aa9
Team10Ariel <team10.car@gmail.com>
2025-05-21 17:21:27 +0300

    changde vo costs

--- src/vo/src/velocity_obstacle.cpp
+++ src/vo/src/velocity_obstacle.cpp
@@ -164,9 +164,9 @@ float VelocityObstacle::calculateCandidateCost(const pose_msg& ego_pose, const t
 
     float cost = 0.0f;
     // cost function constant
-    float obstacle_avoidance_weight = 50.0f;
-    float goal_seeling_weight = 300.0f;
-    float smoothness_weight = 0.0f;
+    float obstacle_avoidance_weight = 100.0f;
+    float goal_seeling_weight = 500.0f;
+    float smoothness_weight = 20.0f;
     
     // Obstacle avoidance 
     pose_msg ego_future_position;
